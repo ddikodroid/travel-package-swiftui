af87581f2dfebd04b90fb24edf568240aada9bda
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-11-23 21:00:04 +0700

    Initial Commit

---
+++ TravelPackage/TravelPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPackageApp.swift
+//  TravelPackage
+//
+//  Created by Ahmad Syarifuddin Randiko on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
